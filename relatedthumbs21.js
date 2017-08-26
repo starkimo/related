c888c687566da8fd19716677b2c6dab0340b4d2c
starkimo <31364397+starkimo@users.noreply.github.com>
2017-08-26 09:46:18 +0200

    Update relatedthumbs21.js

--- relatedthumbs21.js
+++ relatedthumbs21.js
@@ -83,7 +83,7 @@ while (i < relatedTitles.length && i < 20 && i<maxresults) {
 document.write('<a style="text-decoration:none;padding:5px;float:left;');
 if(i!=0) document.write('border-left:solid 0.5px #d4eaf2;"');
 else document.write('"');
-document.write(' href="' + relatedUrls[r] + '"><img style="width:100px;height:100px;border:0px;" src="'+thumburl[r]+'"/><br/><div style="width:100px;padding-left:3px;height:65px;border: 0pt none ; margin: 3px 0pt 0pt; padding: 0pt; font-style: normal; font-variant: normal; font-weight: normal; font-size: 12px; line-height: normal; font-size-adjust: none; font-stretch: normal;">'+relatedTitles[r]+'</div></a>');
+document.write(' href="' + relatedUrls[r] + '"><img style="width:100px;height:100px;border:0px;" src="'+thumburl[r]+'"/><br/><div style="width:100px;padding-left:3px;height:100px;border: 0pt none ; margin: 3px 0pt 0pt; padding: 0pt; font-style: normal; font-variant: normal; font-weight: normal; font-size: 12px; line-height: normal; font-size-adjust: none; font-stretch: normal;">'+relatedTitles[r]+'</div></a>');
 
 
 if (r < relatedTitles.length - 1) {
